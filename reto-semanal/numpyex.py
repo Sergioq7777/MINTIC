66de99a4c30c5d574a0aaefc1c415bc99f29e992
sergioq7777 <1230@holbertonschool.com>
2021-06-23 15:43:22 -0500

    reto3 update

---
+++ reto-semanal/numpyex.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+matriz = [
+    [21,22,23], 
+    [34,35,36], 
+    [47,48,49]
+]
+matriz_np = np.array(matriz)
+
+#print(matriz_np)
+#print(matriz)
+
+#Imprimir la columna
+
+#print(matriz_np[:,0])
+#print(matriz_np[:,1])
+#print(matriz_np[:,2])
+
+#Imprimir Filas
+#print(matriz_np[0][0])
+#print(matriz_np[0][1])
+#print(matriz_np[0][2])
+
+
+#Imprimir en especifico
+#print(matriz_np[1][1])
+#print(matriz_np[1][3])
+
+
+print(matriz_np[0])
