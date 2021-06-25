cbab4176d5dd24941e835b0d5b146f4f175c2c13
sergioq7777 <1230@holbertonschool.com>
2021-06-24 20:04:49 -0500

    reto3-cambiar contraseña

--- reto-semanal/reto3.py
+++ reto-semanal/reto3.py
@@ -1,44 +1,107 @@
 
+usuario = ""
+password = ""
+dic_menu = {
+    "1":"cambiar_contraseña", 
+    "2":"Ingresar_coordenadas_actuales",
+    "3":"Ubicar_zona_wifi_cercana",
+    "4":"Guardar_archivo_ubicacion_cercana",
+    "5":"Actualizar_registros_zonas_wifi_archivo",
+    "6":"Elegir_opcion_menu_favorita",
+    "7":"Cerrar_Sesion"
+    }
 
-def cambiar_contraseña():
-    print("cambiando contraseña".center(30,"*"))
-def Ingresar_coordenadas_actuales():
-    print("Ingresar coordenadas actuales".center(30,"*"))
-def Ubicar_zona_wifi_cercana():
-    print("Ubicar zona wifi más cercana".center(30,"*"))
-def Guardar_archivo_ubicacion_cercana():
-    print("Guardar archivo con ubicación cercana".center(30,"*"))
-def Actualizar_registros_zonas_wifi_archivo():
-    print("Actualizar registros de zonas wifi desde archivo".center(30,"*"))
+def main():
+    if sing_up():
+        login(usuario, password)
+
+
+
+    
+    #(Reto Semanal 1)-RE01
+def sing_up():
+    """
+    *Se imprime la bienvenida
+    *Se genera dos input: 
+    1) input usuario = string de un numero
+    2) input password = string de el numero usuario en reversa
+    """
+
+    #se pone global aqui para poder modificar las variables que estan afuera
+    global usuario 
+    global password
+
+    print("Bienvenido al sistema de ubicación para zonas públicas WIFI")
+    print("*".center(70,"*"))
+    usuario=str(input("Ingrese Nombre de Usuario: "))
+    if len(usuario) == 5:
+        password=str(input("Ingrese contraseña: "))
+        return usuario,password
+        
+    else:
+        print("Error")
+        print("Porfavor digite usuario y clave de 5 digitos")
+        #Usa una funcion recursiva y se vuelve a llamar asi mismo
+        sing_up()
+
+
+
+#(Reto Semanal 1)-RE02,RE03,RE04
+def login(usr, psw):
+    """
+    RE02:
+    Este login recive el input usuario y password y verifica
+    si los dos datos de 6 digitos son iguales y redirecciona al menu inicial.
+    Si no imprime **Error**
+
+    RE03:
+    *El captcha sera el resultado de la suma de dos terminos:
+    
+    terminos:
+    1) Ultimos 3 digitos del usuario
+    2) Penultimo numero del usuario
+
+    RE04:
+    Mostrar en pantalla Sesion iniciada
+    """
+
+    u = usr
+    # Convierte Str en Int y se imprime desde la posicion -1
+    p = psw[::-1]
+    # Convierte Str en Int y arranca de la posicion 3 hasta la 1 en forma inversa
+    ultimos_tres = int(usr[2::1])
+    # Convierte Str en Int y arranca de la posicion 5 hasta la 1
+    ultimo = int(usr[4::1])
+
+    if ultimo == 0:
+        ultimo = int(usr[3::2])
+
+    
+    res = int(ultimo + ultimos_tres)
+    
+    
+    if u == p:
+        print("Captcha".center(30,"*"))
+        print("".center(30,"*"))
+        respuesta_usuario = int(input(f"""
+        {ultimos_tres}+{ultimo}:{res}
+        type the answer:"""))
+        if res == respuesta_usuario:
+            print("Sesión iniciada".center(30,"-"))
+            menu_inicial()
+        else:
+            print("Error")
+    else:
+        print("Error")
+
+
 
 #(Reto Semanal 2)-RE01
 def menu_inicial():
     """
     RE01:
     """
-    
-    list_num = [1,2,3,4,5,6,7]
-    list_options = [
-        "Cambiar contraseña", "Ingresar coordenadas actuales", 
-        "Ubicar zona wifi más cercana", "Guardar archivo con ubicación cercana",
-        "Actualizar registros de zonas wifi desde archivo", "Elegir opción de menú favorita",
-        "Cerrar sesión"
-        ]
-    #First step show the options in a list
-   
-    def print_menu(lis1, lis2):
-        print("MENU DE INICIO".center(30,"*"))
-        i =1
-        j=1
-        for i,j in zip(lis1,lis2):
-            print(i,j)
-    
-
-    print_menu(list_num, list_options)
-    
-
-
-
+    mostrar_menu()
 
     acumulador = 1
     while acumulador <= 3:
@@ -46,30 +109,9 @@ def menu_inicial():
         option = int(input('Elija una opción del menu: '))
         if option in range(1,8):
             if option == 6:
-                    change = int(input('Elija un favorito de la opcion 1 al 5: '))
-                    if change > 5 :
-                            print("Error")
-                            break
-                    else:
-                        res = capchat_adivina()
-                        if res == True:
-                            a = list_options[change]
-                            del list_options[change]
-                            list_options.insert(0,a)
-
-
-                            print_menu(list_num, list_options)
-                            o = int(input('Elija una opción del menu: '))
-                            print(f"o= {o}")
-                            menu_option(o)
-                            break
-                        else:
-                            print("Error")
-                            break
+                Elegir_opcion_menu_favorita()
             elif option == 7:
-                print("Hasta pronto")
-                print("Sesión cerrada".center(30,"-"))
-                exit()
+                Cerrar_Sesion()
             else:
                 menu_option(option)
                 acumulador = 4
@@ -102,23 +144,26 @@ def capchat_adivina():
             menu_inicial()
     else:
         print("Error")
-    
 
+
+
+def mostrar_menu():
+    for p in dic_menu:
+        print(f"{p} {dic_menu[p]}")
 
 
 def menu_option(option):
+    valor = dic_menu.get(str(option))
 
-    print(f"Usted ha elegido la opción {option}")
-
-    if option==1:
-        cambiar_contraseña()
-    elif option==2:
+    if valor=="cambiar_contraseña":
+        cambiar_contrasena(password)
+    elif valor=="Ingresar_coordenadas_actuales":
         Ingresar_coordenadas_actuales()
-    elif option==3:
+    elif valor=="Ubicar_zona_wifi_cercana":
         Ubicar_zona_wifi_cercana()
-    elif option==4:
+    elif valor=="Guardar_archivo_ubicacion_cercana":
         Guardar_archivo_ubicacion_cercana()
-    elif option==5:
+    elif valor=="Actualizar_registros_zonas_wifi_archivo":
         Actualizar_registros_zonas_wifi_archivo()
     else:
         print("Error")
@@ -126,83 +171,69 @@ def menu_option(option):
         menu_inicial()
 
 
-#(Reto Semanal 1)-RE02,RE03,RE04
-def login(user, passw):
-    """
-    RE02:
-    Este login recive el input usuario y password y verifica
-    si los dos datos de 6 digitos son iguales y redirecciona al menu inicial.
-    Si no imprime **Error**
 
-    RE03:
-    *El captcha sera el resultado de la suma de dos terminos:
-    
-    terminos:
-    1) Ultimos 3 digitos del usuario
-    2) Penultimo numero del usuario
+def cambiar_contrasena(password):
+    print("cambiando contraseña".center(30,"*"))
 
-    RE04:
-    Mostrar en pantalla Sesion iniciada
-    """
+    pass_uno = input("Introduce contraseña actual: ")
+    if pass_uno == password:
+        pass_dos = input("Introduce nueva contraseña: ")
+        if pass_dos != password:
+            pass_tres = input("Confirma la nueva contraseña: ")
+            if pass_dos == pass_tres:
+                password = pass_dos
+                print(f"contraseña cambiada con exito {password}")
+                return password
+            else:
+                print("Error")
+        else:
+            print("Error")
+            print("No se puede usar la misma contraseña\n")
+            menu_inicial()
+    else:
+        print("Error")
 
-    u = int(user) 
-    # Convierte Str en Int y se imprime desde la posicion -1
-    p = int(passw[::-1])
-    
 
-    # Convierte Str en Int y arranca de la posicion 3 hasta la 1 en forma inversa
-    ultimos_tres = int(user[2::1])
-    # Convierte Str en Int y arranca de la posicion 5 hasta la 1
-    ultimo = int(user[4::1])
 
-    if ultimo == 0:
-        ultimo = int(user[3::2])
-  
+
+def Ingresar_coordenadas_actuales():
+    print("Ingresar coordenadas actuales".center(30,"*"))
+def Ubicar_zona_wifi_cercana():
+    print("Ubicar zona wifi más cercana".center(30,"*"))
+def Guardar_archivo_ubicacion_cercana():
+    print("Guardar archivo con ubicación cercana".center(30,"*"))
+def Actualizar_registros_zonas_wifi_archivo():
+    print("Actualizar registros de zonas wifi desde archivo".center(30,"*"))
+
     
-    res = int(ultimo + ultimos_tres)
     
     
-    if u == p:
-        print("Captcha".center(30,"*"))
-        print("".center(30,"*"))
-        respuesta_usuario = int(input(f"""
-        {ultimos_tres}+{ultimo}:{res}
-        type the answer:"""))
-        if res == respuesta_usuario:
-            print("Sesión iniciada".center(30,"-"))
+#corregir
+def Elegir_opcion_menu_favorita():
+    global dic_menu
+    change = int(input('Elija un favorito de la opcion 1 al 5: '))
+    val = dic_menu.get(str(change))
+    if change > 5 :
+        print("Error")
+    else:
+        res = capchat_adivina()
+        if res == True:
+            dic_menu = {**dic_menu,"1":val}
+            dic_menu = {**dic_menu,str(change):"cambiar_contraseña"}
             menu_inicial()
         else:
             print("Error")
-    else:
-        print("Error")
+
+def Cerrar_Sesion():
+    print("Hasta pronto")
+    print("Sesión cerrada".center(30,"-"))
+    exit()
 
 
 
 
 
-#(Reto Semanal 1)-RE01
-def sing_up():
-    """
-    *Se imprime la bienvenida
-    *Se genera dos input: 
-    1) input usuario = string de un numero
-    2) input password = string de el numero usuario en reversa
-    """
-
-    print("Bienvenido al sistema de ubicación para zonas públicas WIFI")
-    print("*".center(70,"*"))
-    usuario=str(input("Ingrese Nombre de Usuario: "))
-    if len(usuario) == 5:
-        password=str(input("Ingrese contraseña: "))
-        login(usuario,password)
-    else:
-        print("Error")
-        print("Porfavor digite usuario y clave de 5 digitos")
-        #Usa una funcion recursiva y se vuelve a llamar asi mismo
-        sing_up()
-    
-    
 
 
 
-sing_up()
+main()
--- reto-semanal/test.py
+++ reto-semanal/test.py
@@ -1,55 +1,46 @@
-"""
-num = int(str(12345)[1::])
-print(num)
-"""
-"""
-def pen_ultimo(serial):
-    for i in serial:
-        num = int(i)
-        pen = num-1
 
-    return pen    
+def Elegir_opcion_menu_favorita():
+    return "hola"
 
-def ultimos_tres(serial):
-    for i in serial:
-        tres = int(i[-3:])
-    print(tres)
-    return tres
+def cambiar_contrasena():
+    return "hola"
+
+def Ingresar_coordenadas_actuales():
+    return "hola"
+def Ubicar_zona_wifi_cercana():
+    return "hola"
+def Guardar_archivo_ubicacion_cercana():
+    return "hola"
+def Actualizar_registros_zonas_wifi_archivo():
+    return "hola"
+
+def Cerrar_Sesion():
+    return "sesion cerrada"
+
+dic_menu = {
+    "1":{"cambiar_contraseña":cambiar_contrasena()}, 
+    "2":{"Ingresar_coordenadas_actuales":Ingresar_coordenadas_actuales()},
+    "3":{"Ubicar_zona_wifi_cercana":Ubicar_zona_wifi_cercana()},
+    "4":{"Guardar_archivo_ubicacion_cercana":Guardar_archivo_ubicacion_cercana()},
+    "5":{"Actualizar_registros_zonas_wifi_archivo":Actualizar_registros_zonas_wifi_archivo()},
+    "6":{"Elegir_opcion_menu_favorita":Elegir_opcion_menu_favorita()},
+    "7":{"Cerrar_Sesion":Cerrar_Sesion()}
+    }
+
+def executa(opt):    
+    for i, j in dic_menu.items():
+        if opt == i:
+            return j
+    return "theres is no such key"
 
 
-#(Reto Semanal 1)-RE03
-def captcha(num_usr):
-    
-    *El captcha sera el resultado de la suma de dos terminos:
-
-    terminos:
-    1) Ultimos 3 digitos del usuario
-    2) Penultimo numero del usuario
+def mostrar_menu():
+    for p in dic_menu:
+        for c in dic_menu[p]:
+            #print(f"{p} {c}:{dic_menu[p][c]}")
+            print(f"{p} {c}")
 
 
-    termino_uno = Ticnec.ultimos_tres(num_usr)
-    termino_dos = Ticnec.pen_ultimo(num_usr)
-
-    suma_terminos = termino_uno + termino_dos
-    print(f'{termino_uno}+{termino_dos}={suma_terminos}')
-    
-    return suma_terminos
-"""    
-
-        
-    def contador():
-        conta = 1
-        while conta <= 3:
-            ask = int(input("Type option: "))
-            if ask == 6:
-                print("correcto")
-                conta = 4
-            else:
-                print("Respuesta incorrecta")
-                if conta == 3:
-                    print("comunicate con el admin")
-                conta +=1
-
-    
-
-contador()
+#n = str(input("type a option"))
+#print(executa(n))
+mostrar_menu()
